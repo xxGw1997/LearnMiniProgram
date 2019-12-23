6f8d3d93cac9830794135eb1b196effc48bbcd24
xxgw <540040588@qq.com>
2019-12-23 23:58:51 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,26 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'xxGw',
+    age:18,
+    hobbys:[
+      { id: 1, name: 'games' },
+      { id: 2, name: 'girls' },
+      { id: 3, name: 'sleep' }
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    //错误做法
+    // this.data.counter += 1
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    //2.this.setData()
+    this.setData({
+      counter:this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubClick(){
+    this.setData({
+      counter:this.data.counter - 1
+    })
   }
 })
